ad989160e03f38f5216c1d3695ff8dead07e98b0
CutaiaMattia <matcut94@gmail.com>
2023-04-12 10:46:22 +0200

    add 2 new components to try router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { WizardComponent } from './components/wizard/wizard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:HomepageComponent},
+  {path:"wizard", component:WizardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { WizardComponent } from './components/wizard/wizard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    WizardComponent
   ],
   imports: [
     BrowserModule,
